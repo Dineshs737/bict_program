73b5a28d9def3776cabc80afc761b7cd2ad5752f
Dineshs737 <dinesh77saarck@gmail.com>
2024-02-03 21:44:40 +0530

    update

--- source/Exercise07.c
+++ source/Exercise07.c
@@ -1,15 +1,19 @@
 #include<stdio.h>
 
+//this is simple program for inches to feet conveter
+
 
 int main()
 {
      int inches ;
      const int inches_per_foot = 12;
 
-    printf("you shoud inches more than 12 inches :");
+    printf("you shoud inches more than 12 inches and you don't enter float values :");
 
     scanf("%d",&inches);
-    
+
+    printf("Your Feet is : %d\n",inches/inches_per_foot);
+
 
     return 0;
 }
