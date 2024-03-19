db13d6550492093b8f47aeab5ad62be6aa034cf7
Dineshs737 <dinesh77saarck@gmail.com>
2024-03-19 09:25:23 +0530

    error fixed

--- functions/E2.c
+++ functions/E2.c
@@ -5,31 +5,35 @@
 double diameter(double);
 double circumfrence(double);
 double area(double);
+
 int main()
 {
-    double radius;
+    double radius =7;
 
     printf("Enter the radius :");
-    scanf("%lf",radius);
 
-    printf("\nCircle of the Diameter is %lf",diameter(radius));
+    scanf("%lf",&radius);
+    
+    
 
-    printf("\nCircle of the Circumference is %lf",circumfrence(radius));
+    printf("\nCircle of the Diameter is %.2lf\n",diameter(radius));
 
-    printf("\nCircle of the Area is %lf",area(radius));
+    printf("\nCircle of the Circumference is %.2lf\n",circumfrence(radius));
+
+    printf("\nCircle of the Area is %.2lf\n",area(radius));
 
     return 0;
 }
-double diameter(double radius)
+double diameter(double r)
 {
-    return 2*radius;
+    return 2*r;
 }
-double circumfrence(double radius)
+double circumfrence(double r)
 {
-    return 2*PI*radius;
+    return 2*PI*r;
 }
 
-double area(double radius)
+double area(double r)
 {
-    return PI*radius*radius;
+    return PI*r*r;
 }
